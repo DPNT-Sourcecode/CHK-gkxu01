c067859b49d99fbfb9f1d5edbe77a60784d5b4df
sbx_user1051 <sbx_user1051@169.254.126.45>
2024-06-06 22:00:38 +0000

    Thu Jun 06 22:00:38 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,9 @@
+import re
 
+def is_invalid_input(skus: str) -> bool:
+    regex = re.compile('^[A-D]+$')
+    return True if not regex.match(skus) else False
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
