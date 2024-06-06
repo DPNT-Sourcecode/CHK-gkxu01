6a99d15b5778ae4f11b4246cb89ef83fba7d865a
sbx_user1051 <sbx_user1051@169.254.126.45>
2024-06-06 22:09:38 +0000

    Thu Jun 06 22:09:38 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,11 +6,23 @@ def is_invalid_input(skus: str) -> bool:
     return True if not regex.match(skus) else False
 
 
+def item_price(sku: str):
+    prices = {
+        'A': { 'price': 50, 'special_offer': {'quantity': 3, 'price': 130} },
+        'B': { 'price': 30, 'special_offer': {'quantity': 3, 'price': 130} },
+        'C': { 'price': 20 },
+        'D': { 'price': 15 },
+    }
+
+    return prices.get(sku)
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     if is_invalid_input(skus):
         return -1
+
+
 
     sku_dict = {}
     for item in skus:
@@ -19,6 +31,10 @@ def checkout(skus):
         else:
             sku_dict[item] = 1
 
+    checkout_value = 0
     for item, amount in sku_dict.items():
+        if item == 'A':
+            if amount
+
 
 
