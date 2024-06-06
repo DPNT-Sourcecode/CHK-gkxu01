1e60dfed1c6427df38be1c03d8fb59b5a69aeac3
sbx_user1051 <sbx_user1051@169.254.126.45>
2024-06-06 22:25:58 +0000

    Thu Jun 06 22:25:58 UTC 2024

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -5,9 +5,16 @@ class TestCheckout():
     def test_checkout_with_inavlid_input(self):
         assert checkout_solution.checkout('ABCDEDCBA') == -1
 
-    def test_checkout_when_skus_does_not_have_duplicates(self):
+    def test_checkout_when_skus_does_not_have_repeated_items(self):
         assert checkout_solution.checkout('ABCD') == 115
 
-    def test_checkout_when_skus_does_have_duplicates(self):
-        assert checkout_solution.checkout('ABCD') == 115
+    def test_checkout_when_skus_only_has_repeated_items(self):
+        assert checkout_solution.checkout('AAAAAA') == 260
+
+    def test_checkout_when_skus_has_repeated_items_and_single_items(self):
+        assert checkout_solution.checkout('AAAAAAA') == 310
+
+    def test_checkout_when_skus_has_mixed_items(self):
+        assert checkout_solution.checkout('ABCDABCDADD') == 275
+
 
