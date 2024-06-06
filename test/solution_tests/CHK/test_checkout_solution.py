d036af56a96effa9f595578b06e4fa54acd7c16d
sbx_user1051 <sbx_user1051@169.254.126.45>
2024-06-06 22:24:38 +0000

    Thu Jun 06 22:24:38 UTC 2024

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -4,3 +4,10 @@ from solutions.CHK import checkout_solution
 class TestCheckout():
     def test_checkout_with_inavlid_input(self):
         assert checkout_solution.checkout('ABCDEDCBA') == -1
+
+    def test_checkout_when_skus_does_not_have_duplicates(self):
+        assert checkout_solution.checkout('ABCD') == 115
+
+    def test_checkout_when_skus_does_have_duplicates(self):
+        assert checkout_solution.checkout('ABCD') == 115
+
