77a281ab8ba0692395aae2622652efe2746cead8
sbx_user1051 <sbx_user1051@169.254.126.45>
2024-06-06 22:03:38 +0000

    Thu Jun 06 22:03:38 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,9 +1,23 @@
 import re
 
+
 def is_invalid_input(skus: str) -> bool:
     regex = re.compile('^[A-D]+$')
     return True if not regex.match(skus) else False
 
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    if is_invalid_input(skus):
+        return -1
+
+    sku_dict = {}
+    for item in skus:
+        if sku_dict.get(item) is not None:
+            sku_dict[item] += 1
+        else:
+            sku_dict[item] = 1
+
+    for item, amount in sku_dict:
+
